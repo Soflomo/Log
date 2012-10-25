271f18b1b14f09cfa0776e2ec698760c10bf8d43
Jurian Sluiman <jurian@soflomo.com>
2012-10-25 23:06:51 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+Soflomo\Log
+===
+`Soflomo\Log` is a small module that provides a drop-in configuration, instantiation and initialization of a `Zend\Log\Logger` object.
---
+++ src/Log/Service/LoggerFactory.php
@@ -0,0 +1,68 @@
+<?php
+/**
+ * Copyright (c) 2012 Jurian Sluiman.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *
+ *   * Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in
+ *     the documentation and/or other materials provided with the
+ *     distribution.
+ *
+ *   * Neither the names of the copyright holders nor the names of the
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * @package     Soflomo\Log
+ * @author      Jurian Sluiman <jurian@soflomo.com>
+ * @copyright   2012 Jurian Sluiman.
+ * @license     http://www.opensource.org/licenses/bsd-license.php  BSD License
+ * @link        http://soflomo.com
+ */
+
+namespace Soflomo\Log\Service;
+
+use Zend\ServiceManager\FactoryInterface;
+use Zend\ServiceManager\ServiceLocatorInterface;
+
+class LoggerFactory implements FactoryInterface
+{
+    public function createService(ServiceLocatorInterface $serviceLocator)
+    {
+        $config = $serviceLocator->get('config');
+        $config = $config['soflomo_log'];
+
+        $logger  = new Logger;
+        $plugins = $logger->getWriterPluginManager();
+        foreach ($config['writers'] as $name => $options) {
+            if (!$options['enabled']) {
+                continue;
+            }
+            unset($options['enabled']);
+
+            $writer = $plugins->writerPlugin($name, $options);
+            $logger->addWriter($writer);
+        }
+
+        return $logger;
+    }
+}
